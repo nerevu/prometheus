471b85590af6da803cac2ef6c591569530e32896
Reuben Cummings <reubano@gmail.com>
2011-05-12 17:37:23 +0300

    transitioning away from MDB2_schema

--- sqlite2pgsql.php
+++ sqlite2pgsql.php
@@ -9,12 +9,15 @@ $source_db_user		= trim(shell_exec('whoami'));
 $source_db_pwd 		= '';
 $source_db_host		= 'localhost';
 $source_db_port		= '';
+$source_schema_file	= false;
 $dest_db_type		= 'pgsql';
 $dest_db_user		= '';
 $dest_db_pwd 		= '';
 $dest_db_host		= '';
 $dest_db_port		= '';
+$output_schema_file	= false;
 $check 				= false;
+$errors 			= false;
 
 // include files
 require_once 'Console/CommandLine.php';
@@ -22,6 +25,7 @@ require_once $projects_dir.'/library/error2.inc';
 require_once $projects_dir.'/library/class_general.inc';
 require_once $thisproject_dir.'/include/class_mdb2_schema.inc';
 require_once $thisproject_dir.'/include/class_mdb2.inc';
+require_once $thisproject_dir.'/include/class_array2xml.inc';
 
 // create the parser from xml file
 $xmlfile = $thisproject_dir.'/sqlite2pgsql.xml';
@@ -45,15 +49,14 @@ try {
 	// load options if present
 	if(!$result->options['dest_db_name']) $dest_db_name = $source_db_name;
 	else $dest_db_name = $result->options['dest_db_name'];
-	
-	if(!$result->options['schema_file']) $schema_file = $schema_path.$source_db_name.'.xml';
-	else $schema_file = $result->options['schema_file'];
-	
+		
 	if($result->options['source_db_type']) $source_db_type = $result->options['source_db_type'];
 	if($result->options['source_db_user']) $source_db_user = $result->options['source_db_user'];
 	if($result->options['source_db_pwd']) $source_db_pwd = $result->options['source_db_pwd'];
 	if($result->options['source_db_host']) $source_db_host = $result->options['source_db_host'];
 	if($result->options['source_db_port']) $source_db_port = $result->options['source_db_port'];
+	if($result->options['source_schema_file']) $source_schema_file = $result->options['source_schema_file'];
+	if($result->options['output_schema_file']) $output_schema_file = $result->options['output_schema_file']; 
 	if($result->options['dest_db_type']) $dest_db_type = $result->options['dest_db_type'];
 	
 	if(!$result->options['dest_db_user']) $dest_db_user = $source_db_user;
@@ -87,26 +90,34 @@ try {
 	} //<-- end if -->
 
 	// execute program
-	$connection = new class_mdb2_schema($source_db_name, $source_db_type, $verbose, $source_db_user, $source_db_pwd, $source_db_host, $source_db_port);
+	if(!$source_schema_file) {
+		$connection = new class_mdb2($source_db_name, $source_db_type, $verbose, $source_db_user, $source_db_pwd, $source_db_host, $source_db_port);
+		if($connection->mdb2) $arr_definition = $connection->db_to_def_arr();
+		else $errors = TRUE;
+	} else {
+		$connection = new class_mdb2($dest_db_name, $dest_db_type, $verbose, $dest_db_user, $dest_db_pwd, $dest_db_host, $dest_db_port); // using destination database details so we have something to connect to and can access the xml_to_def_arr() function
+		if($connection->mdb2) $arr_definition = $connection->xml_to_def_arr($source_schema_file);
+		else $errors = TRUE;
+	} //<-- end if -->
 	
-	if($connection) {
-		//$check = $connection->db_to_xml($schema_file);
-		$check = true;
-		if($check) {
-			//$connection = new class_mdb2($dest_db_name, $dest_db_type, $verbose, $dest_db_user, $dest_db_pwd, $dest_db_host, $dest_db_port);
-			if($connection) {
-				//$old_definition = $connection->db_to_def_arr();
-				$old_definition = $connection->xml_to_def_arr($schema_file);
-				$connection = new class_mdb2($source_db_name, $source_db_type, $verbose, $source_db_user, $source_db_pwd, $source_db_host, $source_db_port);
-				$new_definition = $connection->convert_def_arr($old_definition);
-				//print_r($new_definition);
-				$connection->def_arr_to_db($new_definition);
-				
+	if(!$errors) {
+		if($arr_definition) {
+			if($output_schema_file) {
+				$array2xml = new class_array2xml();
+				$xml = $array2xml->createNode($arr_definition);
+				$xml_file = $schema_path.$dest_db_name.'.xml';
+				$check = $general->write2file($xml, $xml_file);
 				if(!$check) $errors = TRUE;
-			} else $errors = TRUE;	
-		} else $errors = TRUE;
-	} else $errors = TRUE;
-
+			} else {
+				if(!$source_schema_file) $connection = new class_mdb2($dest_db_name, $dest_db_type, $verbose, $dest_db_user, $dest_db_pwd, $dest_db_host, $dest_db_port);
+				if($connection->mdb2) {
+					$check = $connection->def_arr_to_db($arr_definition);
+					if(!$check) $errors = TRUE;
+				} else $errors = TRUE;
+			} //<-- end if -->
+		} else $errors = TRUE;	
+	} //<-- end if errors -->
+	
 	if(!$errors) {
 		if($verbose) fwrite(STDOUT, "Program $program completed successfully!\n");
 		exit(0);
@@ -114,7 +125,25 @@ try {
 		fwrite(STDERR, "Program $program completed with errors.\n");
 		exit(1);
 	} //<-- end if errors -->
-	
+
+	//*************************************
+	// sqlite2pgsql testing
+	//
+	// mysql -> schema file		| sqlite2pgsql -fvs mysql -p stockman04 test
+	// postgres -> schema file	| sqlite2pgsql -fvs pgsql -p stockman04 test
+	// sqlite -> schema file	| sqlite2pgsql -fvp stockman04 test
+	// schema file -> mysql		| sqlite2pgsql -Fvd mysql -P stockman04 /path/test.xml
+	// schema file -> postgres	|
+	// schema file -> sqlite	|
+	// mysql -> postgres		|
+	// mysql -> sqlite			|
+	// postgres -> mysql		|
+	// postgres -> sqlite		|
+	// sqlite -> mysql			|
+	// sqlite -> postgres		|
+	//
+	//*************************************
+		
 	//*************************************
 	// End sqlite2pgsql 
 	//*************************************
