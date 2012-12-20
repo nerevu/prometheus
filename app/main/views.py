2ab7d34fb7b8afcca00d4278575178b77d8ef855
Reuben Cummings <reubano@gmail.com>
2012-12-21 00:00:26 +0300

    Add about page

--- app/main/views.py
+++ app/main/views.py
@@ -6,3 +6,7 @@ main = Blueprint('main', __name__)
 def home():
 	return render_template('main/home.html')
 
+@main.route('/about/', methods=['GET', 'POST'])
+def about():
+	return render_template('main/about.html')
+
