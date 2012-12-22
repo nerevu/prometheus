4e756888f9e8d8fbc75a21cf4663bbfe1e14094e
Reuben Cummings <reubano@gmail.com>
2012-12-22 03:10:05 +0300

    Add more tables

--- app/hermes/models.py
+++ app/hermes/models.py
@@ -58,3 +58,42 @@ class Event(db.Model, ValidationMixin):
 	def __repr__(self):
 		return ('<Event(%r, %r, %r, %r)>'
 			% (self.symbol, self.value, self.event_type_id, self.date))
+
+class Price(db.Model, ValidationMixin):
+	__table_args__ = (db.UniqueConstraint('commodity_id', 'currency_id',
+		'date'), {})
+
+	id = db.Column(db.Integer, primary_key=True)
+	utc_created = db.Column(db.DateTime, nullable=False, default=dt.utcnow())
+	utc_updated = db.Column(db.DateTime, nullable=False, default=dt.utcnow(),
+		onupdate=dt.utcnow())
+
+	commodity_id = db.Column(db.Integer, db.ForeignKey('commodity.id'), nullable=False)
+	commodity = db.relationship('Commodity', backref='commodity_prices', lazy='joined')
+	currency_id = db.Column(db.Integer, db.ForeignKey('currency.id'), nullable=False)
+	currency = db.relationship('Commodity', backref='currency_prices', lazy='joined')
+	date = db.Column(db.Date, nullable=False, default=d.today())
+	close = db.Column(db.Float, nullable=False)
+
+	# validation
+	val.validates_constraints()
+
+	def __repr__(self):
+		return ('<Price(%r, %r, %r, %r)>'
+			% (self.close, self.commodity_id, self.currency_id, self.date))
+
+class Commodity(db.Model, ValidationMixin):
+	id = db.Column(db.Integer, primary_key=True)
+	utc_created = db.Column(db.DateTime, nullable=False, default=dt.utcnow())
+	utc_updated = db.Column(db.DateTime, nullable=False, default=dt.utcnow(),
+		onupdate=dt.utcnow())
+
+	cusip = db.Column(db.String(16), unique=True, nullable=False)
+	symbol = db.Column(db.String(12), unique=True, nullable=False)
+	name = db.Column(db.String(64), nullable=False, unique=True)
+
+	# validation
+	val.validates_constraints()
+
+	def __repr__(self):
+		return ('<Commodity(%r, %r)>' % (self.symbol, self.name))
