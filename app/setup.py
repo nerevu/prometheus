71e3d36cab0965b8727bd222c7da61dce24e988a
Reuben Cummings <reubano@gmail.com>
2013-01-08 16:00:19 +0300

    Edit setup

--- app/setup.py
+++ app/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
-setup(name='Hermes',
-	version='0.5',
+setup(name='Prometheus',
+	version='0.8',
 	long_description=__doc__,
 	packages=find_packages(),
 	include_package_data=True,
