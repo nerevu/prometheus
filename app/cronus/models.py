fa0b745d656937f62ae394e5edb9821f555f377c
Reuben Cummings <reubano@gmail.com>
2013-01-13 23:19:51 +0300

    Remove event listener

--- app/cronus/models.py
+++ app/cronus/models.py
@@ -9,18 +9,7 @@ from app.hermes.models import Commodity
 
 from savalidation import ValidationMixin
 from flask.ext.sqlalchemy import SQLAlchemy
-from sqlalchemy import event
-from sqlalchemy.engine import Engine
 from sqlalchemy.orm import backref
-
-
-@event.listens_for(Engine, 'connect')
-def set_sqlite_pragma(dbapi_connection, connection_record):
-	if 'sqlite3' in str(dbapi_connection.cursor):
-		"""Enable foreign key constraints for SQLite."""
-		cursor = dbapi_connection.cursor()
-		cursor.execute("PRAGMA ON")
-		cursor.close()
 
 
 class Person(db.Model, ValidationMixin):
