706bc25ebe50fab95fa0bfe98eaf8cdd900740d6
Reuben Cummings <reubano@gmail.com>
2013-01-14 17:09:31 +0300

    Refactor script manager

--- manage.py
+++ manage.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
+import os.path as p
+
+from subprocess import call, check_output
 from pprint import pprint
-from os.path import abspath
 
 from flask import current_app as app, url_for
 from flask.ext.script import Manager
@@ -10,7 +12,7 @@ from app.manage_helper import init_db, pop_db
 manager = Manager(create_app)
 manager.add_option(
 	'-m', '--cfgmode', dest='config_mode', default='Development')
-manager.add_option('-f', '--cfgfile', dest='config_file', type=abspath)
+manager.add_option('-f', '--cfgfile', dest='config_file', type=p.abspath)
 
 
 def get_api_endpoint():
@@ -25,39 +27,48 @@ def get_api_endpoint():
 
 
 @manager.command
-def createdb():
-	with app.app_context():
+def checkstage():
+	"""Checks staged with git pre-commit hook"""
 
-		"""Creates database if it doesn't already exist"""
+	path = p.join(p.dirname(__file__), 'app', 'tests', 'test.sh')
+	cmd = "sh %s" % path
+	return call(cmd, shell=True)
+
+
+@manager.command
+def createdb():
+	"""Creates database if it doesn't already exist"""
+
+	with app.app_context():
 		db.create_all()
 		print 'Database created'
 
 
 @manager.command
 def cleardb():
-	with app.app_context():
+	"""Removes all content from database"""
 
-		"""Removes all content from database"""
+	with app.app_context():
 		db.drop_all()
 		print 'Database cleared'
 
 
 @manager.command
 def resetdb():
-	with app.app_context():
+	"""Removes all content from database and creates new tables"""
 
-		"""Removes all content from database and creates new tables"""
+	with app.app_context():
 		cleardb()
 		createdb()
 
 
 @manager.command
 def initdb():
-	with app.app_context():
-
-		"""Removes all content from database and initializes it
+	"""Removes all content from database and initializes it
 		with default values
-		"""
+	"""
+
+	with app.app_context():
 		resetdb()
 		r = init_db(get_api_endpoint())
 		print 'Database initialized'
@@ -65,11 +76,11 @@ def initdb():
 
 @manager.command
 def popdb():
-	with app.app_context():
-
-		"""Removes all content from database and populates it
+	"""Removes all content from database and populates it
 		with sample data
-		"""
+	"""
+
+	with app.app_context():
 		initdb()
 		r = pop_db(get_api_endpoint())
 		print 'Database populated'
