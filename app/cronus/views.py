7fd0b4fce3d52498132809ea0af5643ca7b7ee69
Reuben Cummings <reubano@gmail.com>
2013-01-31 00:07:25 +0300

    Create cronus views

--- app/cronus/views.py
+++ app/cronus/views.py
@@ -21,4 +21,34 @@ def transaction():
 
 @cronus.route('/add_trxn/', methods=['GET', 'POST'])
 def add():
-	pass
+	table_as_class = table.title().replace('_', '')
+
+	try:
+		form = eval('%sForm.new()' % table_as_class)
+	except AttributeError:
+		form = eval('%sForm()' % table_as_class)
+
+	if form.validate_on_submit():
+		site = portify(url_for('api', _external=True))
+		conn = Connection(site, display=True)
+		entry = eval('%s()' % table_as_class)
+		form.populate_obj(entry)
+		keys = [f for f in form._fields.keys() if f != 'csrf_token']
+		values = [getattr(form, k).data for k in keys]
+		content = conn.process(values, table, keys)
+		conn.post(content)
+		flash(
+			'Awesome! You just posted a new %s.' % table.replace('_', ' '),
+			'alert alert-success')
+
+	else:
+		[flash('%s: %s.' % (k.title(), v[0]), 'alert alert-error')
+			for k, v in form.errors.iteritems()]
+
+	return redirect(url_for('.transaction'))
+
+
+@cronus.errorhandler(409)
+def duplicate_values(e):
+	flash('Error: %s' % e.orig[0], 'alert alert-error')
+	return redirect(url_for('.transaction'))
