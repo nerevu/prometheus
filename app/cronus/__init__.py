33817a15c6ad8f53b7408ec094d9e79ffb472b0a
Reuben Cummings <reubano@gmail.com>
2013-02-06 10:18:54 -0400

    Add documentation

--- app/cronus/__init__.py
+++ app/cronus/__init__.py
@@ -12,14 +12,67 @@ from app.connection import Connection
 
 
 class DataSource(Connection):
+	"""
+	Generic data source.
+	"""
 	def __init__(self, site, native=1, display=False):
+		"""Creates a DataSource
+
+		Parameters
+		----------
+		site : a string
+			api endpoint
+
+		native : a number, default 1
+			id of the native currency
+
+		display : boolean, default False
+
+		Examples
+		--------
+		>>> DataSource('http://localhost:5000/api/')  # doctest: +ELLIPSIS
+		<app.cronus.DataSource object at 0x...>
+		"""
 		super(DataSource, self).__init__(site, native, display)
 
 
 class CSV(DataSource):
+	"""
+	CSV data source.
+
+	Attributes
+	----------
+	content : list of dicts
+		csv contents
+	"""
 	def __init__(
 			self, file, site, native=1, display=False, delimiter=',',
 			quotechar='"'):
+		"""Creates a CSV DataSource
+
+		Parameters
+		----------
+		file : a string
+			the csv location
+
+		site : a string
+			api endpoint
+
+		native : a number, default 1
+			id of the native currency
+
+		display : boolean, default False
+		delimiter : a character, default ','
+			the csv field delimiter
+
+		quotechar : a character, default '"'
+			the text quote character
+
+		Examples
+		--------
+		>>> CSV('file.csv', 'http://localhost:5000/api/')  # doctest: +ELLIPSIS
+		<app.cronus.CSV object at 0x...>
+		"""
 		self.file = file
 		self.delimiter = delimiter
 		self.quotechar = quotechar
